16bdd575d81cb4d90814b2961026b52018ee52f2
«Aliya <alyakalimova@mail.ru>
2022-07-02 15:47:44 +0600

    hometask done

--- task2419/Hometask/Program.cs
+++ task2419/Hometask/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B\
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// Console.WriteLine("Введите число а");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число b");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// double ab = Math.Pow(a, b);
+// Console.WriteLine($"Число А в натуральной степени числа В {ab}");
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int a = int.Parse(Console.ReadLine());
+// int s = 0;
+// while (a > 0)
+// {
+// s = s + a % 10;
+// a = a /10 ;
+// }
+// Console.WriteLine(s);
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int [] massiv = new int [8];
+// void masmetod (int [] masother){
+//     Console.Write("Массив = [");
+//     for (int i = 0; i <= masother.Length - 2; i++){
+//         masother[i] = new Random().Next(0,2);
+//         Console.Write($"{masother[1]}, ");
+//         }
+//         masother[masother.Length - 1] = new Random().Next(0,2);
+//         Console.Write($"{masother[masother.Length - 1]}");
+//         Console.Write("]");     
+        
+//     }
+// masmetod(massiv);
